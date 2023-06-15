bc517273299099c57425d6d2f23e05d9508950cb
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-15 17:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",(e)=>{
+	let p = document.createElement("p");
+	p.textContent="DOM load success"
+	document.body.appendChild(p)
+})
